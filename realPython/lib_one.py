aff510a61a47c6aa610312824de3dff3a1137127
cmoz <cmozruts@gmail.com>
2020-11-17 19:36:16 +0300

    update python library:merge dict, sort dict, test multiple flags, get() method for dict, namedtuple(define class manually)

--- realPython/lib_one.py
+++ realPython/lib_one.py
@@ -15,6 +15,7 @@ my_car
 
 #namedtuples are immutable
 
+####
 #The get() method on Python dicts and its "default" arg arguments
 
 name_for_userid = {
@@ -31,3 +32,54 @@ greeting(234)
 # Hi alice
 greeting(988)
 #Hi there
+
+###
+#How to sort a Python dict (dictionary) by value
+# (== get a representation sorted by value)
+
+xs = {'a':4, 'b':3, 'c':2, 'd':1}
+sorted(xs.items(), key=lambda x:x[1])
+#[('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+#or
+import operator
+sorted(xs.items(),key=operator.itemgetter(1))
+#[('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+
+###
+##Merging two dicts (dicrionaries) in Python 3.5+ with a single expression
+
+x = {'a': 1, 'b': 2}
+y = {'b': 3, 'c': 4}
+z = {**x, **y}
+z
+#{'c': 4, 'a': 1, 'b': 3}
+
+# In Python 2.x you could use this
+z = dict(x, **y)
+z
+#{'a': 1, 'c': 4, 'b': 
+# In these examples, Python merges dictionary keys
+# in the order listed in the expression, overwriting 
+# duplicates from left to right.
+
+####
+#Different ways to test multiple flags at once in Python
+# Different ways to test multiple
+# flags at once in Python
+x, y, z = 0, 1, 0
+if x == 1 or y==1 or z==1:
+	print('passed')
+
+if 1 in (x,y,z):
+	print('passed')
+
+#these only test for truthiness:
+if x or y or z:
+	print('passed')
+
+if any((x,y,z)):
+	print('passed')
+
+
+
+
