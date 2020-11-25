4484964577efcadc0b07d0a346f8a7694a422660
cmoz <cmozruts@gmail.com>
2020-11-25 09:02:17 +0300

    initial commit lib_two: import from lib_one library: using json.dumps(), prettyPrint dicts, implementation of builtin pprint module, function argument unpacking)

--- realPython/lib_one.py
+++ realPython/lib_one.py
@@ -2,7 +2,8 @@
 #Author:@Cmoz
 #Nov 17 2020
 
-
+##################################################
+##################################################
 #Python's namedtuples can be a great alternative to defining a class manually
 
 from collections import namedtuple
@@ -15,7 +16,8 @@ my_car
 
 #namedtuples are immutable
 
-####
+###########################################################
+##################################################
 #The get() method on Python dicts and its "default" arg arguments
 
 name_for_userid = {
@@ -33,7 +35,7 @@ greeting(234)
 greeting(988)
 #Hi there
 
-###
+#################################################
 #How to sort a Python dict (dictionary) by value
 # (== get a representation sorted by value)
 
@@ -45,7 +47,8 @@ import operator
 sorted(xs.items(),key=operator.itemgetter(1))
 #[('d', 1), ('c', 2), ('b', 3), ('a', 4)]
 
-###
+###########################################################
+#######################################################
 ##Merging two dicts (dicrionaries) in Python 3.5+ with a single expression
 
 x = {'a': 1, 'b': 2}
@@ -62,7 +65,8 @@ z
 # in the order listed in the expression, overwriting 
 # duplicates from left to right.
 
-####
+########################################################
+#######################################################
 #Different ways to test multiple flags at once in Python
 # Different ways to test multiple
 # flags at once in Python
@@ -80,6 +84,62 @@ if x or y or z:
 if any((x,y,z)):
 	print('passed')
 
+####################################################
+###########################################
+## Using "json.dumps()" to pretty-print Python dicts
+## alternative to the recommended pprint module
 
+my_mapping = {'a':21, 'b':13, 'c':0xc0ffee}
+my_mapping
+##{'b': 42, 'c': 12648430. 'a': 23}  # 😞
 
+# the json module can do a much better job:
+import json
+print(json.dumps(my_mapping, indent=4, sort_keys=True))
 
+#{
+#    "a": 23,
+#    "b": 42,
+#    "c": 12648430
+#}
+
+#note this only works with dicts containing
+#primitive types
+#Note: stick to "print" module
+
+##implementation of pprint built in module
+
+import pprint
+stuff = ['spam', 'eggs', 'lumberjack', 'knights', 'nii']
+stuff.insert(0, stuff[:])
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(stuff)
+
+#[   ['spam', 'eggs', 'lumberjack', 'knights', 'ni'],
+#   'spam',
+#   'eggs',
+#    'lumberjack',
+#    'knights',
+#    'ni']
+
+pp.pprint.PrettyPrinter(width=41, compact=True) #depth=6
+pp.pprint(stuff)
+#[['spam', 'eggs', 'lumberjack',
+# 'knights', 'ni'],
+# 'spam', 'eggs', 'lumberjack', 'knights',
+#'ni']
+#
+#
+
+#################################
+################################
+#Function argument unpacking
+def myfunc(x,y,z):
+	print(x,y,z)
+
+tuple_vec=(1,0,1)
+dict_vec={'x':1,'y':0,'z':1}
+myfunc(*tuple_vec)
+##1,0,1
+myfunc(**dict_vec)
+##1,0,1
